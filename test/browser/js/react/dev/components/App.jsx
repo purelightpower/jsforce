5a2c0bd0cfed64e498235bd8fa7651ae6759138d
Morgan Billingsley <mbillingsley@purelightpower.com>
2022-07-27 10:48:56 -0700

    Build Bones for React Test

---
+++ test/browser/js/react/dev/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";;
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello World!</h1>
+                <p>Welcome to the test JSForce React App.</p>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ test/browser/js/react/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+const babelConfigFile = path.resolve(__dirname, "babel.config.json");
+
+module.exports = {
+    entry: path.resolve(__dirname, "dev/index.jsx"),
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env"],
+                    configFile: babelConfigFile,
+                },
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"],
+                    configFile: babelConfigFile,
+                },
+            },
+        ],
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    output: {
+        path: path.resolve(__dirname, "prod/"),
+        filename: "bundle.js",
+    },
+};
