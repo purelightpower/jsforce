b7c2ef48759730c924e81755ca31a8791ad0b824
Morgan Billingsley <mbillingsley@purelightpower.com>
2022-07-27 15:25:54 -0700

    Added Accounts Query to React Test

--- test/browser/js/react/dev/components/App.jsx
+++ test/browser/js/react/dev/components/App.jsx
@@ -6,6 +6,7 @@ class App extends React.Component {
         super(props);
         this.state = {
             user: null,
+            accounts: null,
         };
         jsforce.browser.init({
             clientId: process.env.SALESFORCE_CONSUMER_KEY,
@@ -15,30 +16,58 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        jsforce.browser.on("connect", this.getUser.bind(this));
+        jsforce.browser.on("connect", this.getData.bind(this));
         if (jsforce.browser.isLoggedIn()) {
-            this.getUser(jsforce.browser.connection);
+            this.getData(jsforce.browser.connection);
         }
     }
 
+    getData(connection) {
+        this.getUser(connection)
+            .catch(() => Promise.resolve())
+            .then(() => this.getAccounts(connection));
+    }
+
     getUser(connection) {
-        connection.identity((error, response) => {
-            if (error) {
-                return;
-            }
-            this.setState({
-                user: response,
+        return new Promise((resolve, reject) => {
+            connection.identity((error, response) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    this.setState({
+                        user: response,
+                    });
+                    resolve(response);
+                }
             });
         });
     }
 
+    getAccounts(connection) {
+        return new Promise((resolve, reject) => {
+            connection.query(
+                "SELECT Id, Name FROM Account LIMIT 20",
+                (err, res) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    this.setState({
+                        accounts: res.records,
+                    });
+                    resolve(res.records);
+                }
+            );
+        });
+    }
+
     render() {
+        const loggedIn = jsforce.browser.isLoggedIn();
         return (
             <div className="App">
                 <h1>JSForce React Test</h1>
                 <p>
                     <span>Welcome to the JSForce React test.</span>
-                    {jsforce.browser.isLoggedIn() ? (
+                    {loggedIn ? (
                         this.state.user ? (
                             <span>
                                 You are currently logged in as
@@ -60,6 +89,13 @@ class App extends React.Component {
                         </span>
                     )}
                 </p>
+                <ul>
+                    {loggedIn &&
+                        this.state.accounts &&
+                        this.state.accounts.map((account, index) => (
+                            <li key={index}>{account.Name}</li>
+                        ))}
+                </ul>
             </div>
         );
     }
